9f101ac682debd9dc8b80ae1f245870525b59eb5
Pusk_xt67@itstepru.onmicrosoft.com <Pusk_xt67@itstepru.onmicrosoft.com>
2021-02-14 20:36:24 +0300

    ДЗ 14.02.2021г.

--- Arrays.cpp
+++ Arrays.cpp
@@ -6,25 +6,31 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-
-	const int SIZE = 5;
-	int arr[SIZE] = {3,5,8};
-	cout << int() << endl;// Возвращает значение по умолчанию
-	for (int i = 0; i < SIZE; i++)//Выводим элементы массива на экран
+	int n;
+	int i;
+	const int SIZE = 10; // Объявляем массив
+	int arr[SIZE] = {1,2,3,4,5,6,7,8,9,10}; // Инициализируем элементы массива
+	cout << endl << "  " << "Выводим исходный массив на экран: \n" << endl;
+	for (i = 0; i < SIZE; i++)//Выводим элементы массива на экран
+	{		
+		cout << "  " << arr[i] << tab;
+	}
+	cout << "\n" << endl;
+	cout << "  Введите число на сколько элементов сдвинуть массив: \n"; 
+	cout << endl << "  "; cin >> n; cout << endl;
+	if (n == 0)
 	{
-		cout << arr[i] << tab;
+		for (i = 0; i < 10; i++) cout << "  " << arr[i] << tab; cout << endl;// Если массив не сдвигаем
+	}
+	else if (n != 0) // Сдвигаем массив на заданное число
+	{
+		cout << "  Сдвигаем массив на " << n << " влево: \n"; cout << endl;
+		for (i = n; i < 10; i++) cout << "  " << arr[i] << tab;
+		for (i = 0; i < n; i++)	cout << "  " << arr[i] << tab;
+		cout << "\n" << endl;
+		cout << "  Сдвигаем массив на " << n << " вправо: \n"; cout << endl;
+		for (i = n; i >= 0; i--)	cout << "  " << arr[i] << tab;
+		for (i = 1; i < 10-n; i++) cout << "  " << arr[i] << tab;
 	}
 	cout << endl;
-	cout << "Введите значения элементов массива: \n";
-	for (int i = 0; i < SIZE; i++)
-	{
-		cin >> arr[i];//Вводим значение текущего элемента
-	//Вывод массива на экран в обратном порядке
-	}
-		for (int i = SIZE-1; i >= 0; i--)
-		{
-			cout << arr[i] << tab;
-		}
-		cout << endl;
-	
 }	
